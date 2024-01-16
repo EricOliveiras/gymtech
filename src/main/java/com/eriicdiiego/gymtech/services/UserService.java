042c84c45b23367ca493bdbd150c000042710848
Eric Oliveira <oiericoi@hotmail.com>
2024-01-16 11:19:13 -0300

    feat: add user service

---
+++ src/main/java/com/eriicdiiego/gymtech/services/UserService.java
@@ -0,0 +1,25 @@
+package com.eriicdiiego.gymtech.services;
+
+import com.eriicdiiego.gymtech.entities.User;
+import com.eriicdiiego.gymtech.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+  @Autowired
+  private UserRepository repository;
+
+  public List<User> findAll() {
+    return repository.findAll();
+  }
+
+  public User find(Long id) {
+    Optional<User> user = repository.findById(id);
+
+    return user.orElse(null);
+  }
+}
