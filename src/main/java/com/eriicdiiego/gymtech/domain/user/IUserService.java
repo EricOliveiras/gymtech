20b654d9b0192dcf7cb7fd14c7294205fedaefed
Eric Oliveira <oiericoi@hotmail.com>
2024-01-17 14:33:58 -0300

    feat: add user service interface

---
+++ src/main/java/com/eriicdiiego/gymtech/domain/user/IUserService.java
@@ -0,0 +1,14 @@
+package com.eriicdiiego.gymtech.domain.user;
+
+import com.eriicdiiego.gymtech.domain.user.payload.request.CreateUserRequest;
+import com.eriicdiiego.gymtech.domain.user.payload.response.UserResponse;
+
+import java.util.List;
+
+public interface IUserService {
+  UserResponse save(CreateUserRequest createUserRequest);
+
+  UserResponse find(Long id);
+
+  List<UserResponse> findAll();
+}
