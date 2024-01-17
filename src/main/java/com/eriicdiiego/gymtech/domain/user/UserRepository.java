95b9cc2e143fff1bcc00632828ffcfc5032e3936
Eric Oliveira <oiericoi@hotmail.com>
2024-01-17 14:31:41 -0300

    refactor: add repository annotation

--- src/main/java/com/eriicdiiego/gymtech/domain/user/UserRepository.java
+++ src/main/java/com/eriicdiiego/gymtech/domain/user/UserRepository.java
@@ -1,7 +1,8 @@
 package com.eriicdiiego.gymtech.domain.user;
 
-import com.eriicdiiego.gymtech.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
