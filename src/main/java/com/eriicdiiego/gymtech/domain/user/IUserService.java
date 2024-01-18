4e7657d7c60b7b334b2d5ec2e6966815e8f2a162
Eric Oliveira <oiericoi@hotmail.com>
2024-01-18 13:18:57 -0300

    refactor: modify user service interface

--- src/main/java/com/eriicdiiego/gymtech/domain/user/IUserService.java
+++ src/main/java/com/eriicdiiego/gymtech/domain/user/IUserService.java
@@ -3,6 +3,7 @@ package com.eriicdiiego.gymtech.domain.user;
 import com.eriicdiiego.gymtech.domain.user.payload.request.CreateUserRequest;
 import com.eriicdiiego.gymtech.domain.user.payload.response.UserResponse;
 
+import java.text.ParseException;
 import java.util.List;
 
 public interface IUserService {
