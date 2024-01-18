703f94d2e4ceb24e188d9727caff6233127c935b
Eric Oliveira <oiericoi@hotmail.com>
2024-01-18 13:20:38 -0300

    refactor: modify user service impl

--- src/main/java/com/eriicdiiego/gymtech/domain/user/UserServiceImpl.java
+++ src/main/java/com/eriicdiiego/gymtech/domain/user/UserServiceImpl.java
@@ -2,20 +2,25 @@ package com.eriicdiiego.gymtech.domain.user;
 
 import com.eriicdiiego.gymtech.domain.user.payload.request.CreateUserRequest;
 import com.eriicdiiego.gymtech.domain.user.payload.response.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements IUserService {
   private final UserRepository userRepository;
   private final UserMapper userMapper;
+  private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
   public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
     this.userRepository = userRepository;
@@ -25,6 +30,8 @@ public class UserServiceImpl implements IUserService {
   @Override
   public UserResponse save(CreateUserRequest createUserRequest) {
     User user = userMapper.toEntity(createUserRequest);
+    user.password = hashPassword(user.getBirthday());
+    user.registration = generateRegistration();
     return userMapper.toDto(userRepository.save(user));
   }
 
@@ -50,5 +57,29 @@ public class UserServiceImpl implements IUserService {
       .orElseThrow(
         () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")
       );
+  }
+
+  private String hashPassword(String password) {
+    String birthdayToPassword = convertBirthdayToPassword(password);
+    return passwordEncoder.encode(birthdayToPassword);
+  }
+
+  private String convertBirthdayToPassword(String birthday) {
+    String[] parts = birthday.split("/");
+    return parts[0] + parts[1].substring(2) + parts[2].substring(2);
+  }
+
+  public static String generateRegistration() {
+    int currentYear = java.time.Year.now().getValue();
+
+    String uniqueIdentifier = generateUniqueIdentifier();
+
+    return currentYear + uniqueIdentifier;
+  }
+
+  public static String generateUniqueIdentifier() {
+    Random random = new Random();
+    int randomValue = random.nextInt(1000);
+    return String.format("%03d", randomValue);
   }
 }
