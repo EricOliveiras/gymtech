483042d7d50f5658522eb7f102b0e0d8cb35db50
Eric Oliveira <oiericoi@hotmail.com>
2024-01-20 12:33:08 -0300

    feat: add get all and get by id methods in user controller

--- src/main/java/com/eriicdiiego/gymtech/domain/user/UserController.java
+++ src/main/java/com/eriicdiiego/gymtech/domain/user/UserController.java
@@ -4,12 +4,9 @@ import com.eriicdiiego.gymtech.domain.user.payload.request.CreateUserRequest;
 import com.eriicdiiego.gymtech.domain.user.payload.response.UserResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.text.ParseException;
+import java.util.List;
 
 @RequestMapping("/api/v1/users")
 @RestController
@@ -24,5 +21,15 @@ public class UserController {
   public ResponseEntity<UserResponse> save(@RequestBody CreateUserRequest createUserRequest) {
     UserResponse userResponse = service.save(createUserRequest);
     return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<UserResponse>> getAll() {
+    return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<UserResponse> getByID(@PathVariable Long id) {
+    return ResponseEntity.status(HttpStatus.OK).body(service.find(id));
   }
 }
